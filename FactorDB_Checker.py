5a3e90c58c7809099dcfec445d49adc7f67eab57
R4GN4R <jimchr2001@gmail.com>
2023-03-31 14:22:52 +0300

    Update FactorDB_Checker.py

--- FactorDB_Checker.py
+++ FactorDB_Checker.py
@@ -1,50 +1,52 @@
 from requests import get
 import re
+from simpleeval import simple_eval
+
 def factordb(n: int):
-	"""
-	n (int) - required: Number to be factored.
-	"""
-	
-	try:
-		int(n)
-	except ValueError:
-		return ValueError("n must be integer.")
+    """
+    n (int) - required: Number to be factored.
+    """
+    
+    try:
+        int(n)
+    except ValueError:
+        return ValueError("n must be integer.")
 
-	def FindNumberById(id):
-		try:
-			res = get(f'http://factordb.com/index.php?id={str(id)}').text
-		except Exception as e:
-			return e
+    def FindNumberById(id):
+        try:
+            res = get(f'http://factordb.com/index.php?id={str(id)}').text
+        except Exception as e:
+            return e
 
-		try:
-			return int(re.findall(r'<input type="text" size=100 name="query" value="(.*)">\n<input type="submit" value="Factorize!">', res)[0])
-		except ValueError:
-			try:
-				return simple_eval(re.findall(r'<input type="text" size=100 name="query" value="(.*)">\n<input type="submit" value="Factorize!">', res)[0].replace('^','**').replace('/','//'))
-			except SyntaxError:
-				return None
-		return None
-	
-	try:
-		res = get(f'http://factordb.com/index.php?query={str(n)}').text
-	except Exception as e:
-		return e
+        try:
+            return int(re.findall(r'<input type="text" size=100 name="query" value="(.*)">\n<input type="submit" value="Factorize!">', res)[0])
+        except ValueError:
+            try:
+                return simple_eval(re.findall(r'<input type="text" size=100 name="query" value="(.*)">\n<input type="submit" value="Factorize!">', res)[0].replace('^','**').replace('/','//'))
+            except SyntaxError:
+                return None
+        return None
+    
+    try:
+        res = get(f'http://factordb.com/index.php?query={str(n)}').text
+    except Exception as e:
+        return e
 
-	factors = re.findall(r'"index\.php\?id=([0-9]*)">', res)
+    factors = re.findall(r'"index\.php\?id=([0-9]*)">', res)
 
-	if len(factors) == 0:
-		return None
-	elif (factors[0] == factors[1]):
-		return [1,n]
-	else:
-		factors.pop(0)
-		for i in range(len(factors)):
-			factors[i] = FindNumberById(factors[i])
-		return factors
+    if len(factors) == 0:
+        return None
+    elif (factors[0] == factors[1]):
+        return [1,n]
+    else:
+        factors.pop(0)
+        for i in range(len(factors)):
+            factors[i] = FindNumberById(factors[i])
+        return factors
 
 while 1:
-	n = input("Number: ")
-	if n == "":
-		break
-	else:
-		print(factordb(n))
+    n = input("Number: ")
+    if n == "":
+        break
+    else:
+        print(factordb(n))
